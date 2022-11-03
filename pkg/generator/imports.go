e258f0f7789cb71c537390812fc1307ba8f40d67
Liam Maguire <liammaguiresoftware@outlook.com>
2022-11-04 08:12:07 +1100

    Delete unused code.

--- pkg/generator/imports.go
+++
@@ -1,8 +0,0 @@
-package generator
-
-import "google.golang.org/protobuf/compiler/protogen"
-
-// move to be protogen.GoIdent
-var _methodImports = []protogen.GoImportPath{"context", "google.golang.org/grpc/codes", "google.golang.org/grpc/status"}
-
-var _testImports = []protogen.GoImportPath{"testing", "context", "testing", "google.golang.org/grpc/codes", "google.golang.org/grpc/status", "github.com/stretchr/testify/assert", "github.com/golang/protobuf/proto"}
