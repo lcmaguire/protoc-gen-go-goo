4626b6a0d386dcaea8d1e42fb88c93ca6e523951
Liam Maguire <liammaguiresoftware@outlook.com>
2022-11-07 08:35:02 +1100

    Server streaming done manually.

--- example/streamingservice/responsestream.go
+++ example/streamingservice/responsestream.go
@@ -1,14 +1,29 @@
 package streamingservice
 
 import (
-	"context"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"time"
 
 	"github.com/lcmaguire/protoc-gen-go-goo/example"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // ResponseStream implements tutorial.StreamingService.ResponseStream.
-func (s *StreamingService) ResponseStream(ctx context.Context, in *example.GreetRequest) (out *example.GreetResponse, err error) {
-	return nil, status.Error(codes.Unimplemented, "yet to be implemented")
+func (s *StreamingService) ResponseStream(req *example.GreetRequest, stream example.StreamingService_ResponseStreamServer) error {
+	ctx := stream.Context()
+	ticker := time.NewTicker(time.Second) // You should set this via config.
+	defer ticker.Stop()
+	for i := 0; i < 5; i++ {
+		if ticker != nil {
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-ticker.C:
+			}
+		}
+		if err := stream.Send(&example.GreetResponse{}); err != nil {
+			return err
+		}
+	}
+	return status.Error(codes.Unimplemented, "yet to be implemented.")
 }
