359cbc5a6f3113b3cad3b98b342027f0d20b9acf
Liam Maguire <liammaguiresoftware@outlook.com>
2022-11-16 06:50:05 +1100

    test auth + cors

--- Makefile
+++ Makefile
@@ -75,10 +75,17 @@ grpc-firebase-get:
     -d '{"name": "testCollection/myname"}' \
     localhost:8080 tutorial.ExampleService/GetExample
 
+grpc-firebase-get-auth:
+	grpcurl \
+    -import-path ./examplefirebase -proto firebase.proto -plaintext \
+	-H "Authorization: Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ3YjE5MTI0MGZjZmYzMDdkYzQ3NTg1OWEyYmUzNzgzZGMxYWY4OWYiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoibGlhbSBlbCBhdXN0cmFsaWFubyIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vYS0vQU9oMTRHaUFjRWV4dEg4UXlRalgySjJXSGJITFItZU45TEdSWHRpa0Z3Qmg9czk2LWMiLCJzdG9yZXMiOnsic3RvcmVOYW1lIjoidGVzdCIsImFkbWluIjp0cnVlfSwiaXNzIjoiaHR0cHM6Ly9zZWN1cmV0b2tlbi5nb29nbGUuY29tL3BiY2MtdGVzdC1lbnYiLCJhdWQiOiJwYmNjLXRlc3QtZW52IiwiYXV0aF90aW1lIjoxNjY4Mzc3MTc0LCJ1c2VyX2lkIjoiSlVOQ3RUa1BJN1oyazUzeE9obTRuSEs3ekJvMiIsInN1YiI6IkpVTkN0VGtQSTdaMms1M3hPaG00bkhLN3pCbzIiLCJpYXQiOjE2NjgzNzcxNzQsImV4cCI6MTY2ODM4MDc3NCwiZW1haWwiOiJtYWduYWxkaW5vcmluaG9AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZ29vZ2xlLmNvbSI6WyIxMTQ0MDUxNTYyNjI2MTg1NTc3NDgiXSwiZW1haWwiOlsibWFnbmFsZGlub3JpbmhvQGdtYWlsLmNvbSJdfSwic2lnbl9pbl9wcm92aWRlciI6Imdvb2dsZS5jb20ifX0.CJRqdmQMuhY8OZqeRede3z06jUw1FmB9-VvKGAWBiDAiUUblKI6_u3Ut9oyRfuZiaMyJu9v31Bg4Qxy8rdw3EuDKeJEy4H4ZIv6SWmoJufaDHYVjeXowLelcOgYtJZD7tYAaNWMUb7bhZhLDH1OkxJqs6cjXqajs5Fj1fJVklQCEoDSr4Qw6_CdWH4WHUK-mqOuWPM4ixAApAqDNKIfOSkCRCkDSR8UtnEemCDH-sbF59E60UW7InvIf_MndhNiEfv8ZBCgU3apajOd9DnUGVryq15TBvL5gTyAELpLWfsjfXguS1Souom0joVumzWtjcR4h-p8r3NxLxypbGmZ0wg" \
+    -d '{"name": "testCollection/myname"}' \
+    localhost:8080 tutorial.ExampleService/GetExample
+
 grpc-firebase-list: 
 	grpcurl \
     -import-path ./examplefirebase -proto firebase.proto -plaintext \
-    -d '{ }' \
+	-d '{ }' \
     localhost:8080 tutorial.ExampleService/ListExamples
 
 grpc-firebase-update: 
--- examplefirebase/cmd/sample/main.go
+++ examplefirebase/cmd/sample/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
+	"time"
+
 	v4 "firebase.google.com/go/v4"
+	"github.com/rs/cors"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/api/option"
-	"log"
-	"net/http"
 
 	// your protoPathHere
 	"github.com/lcmaguire/protoc-gen-go-goo/examplefirebase/sampleconnect"
@@ -20,6 +23,8 @@ func main() {
 	mux := http.NewServeMux()
 	// The generated constructors return a path and a plain net/http
 	// handler.
+
+	newCORS().Handler(mux)
 	mux.Handle(sampleconnect.NewExampleServiceHandler(createNewService()))
 	// export FIREBASE_AUTH_EMULATOR_HOST="localhost:9099"
 	// export FIRESTORE_EMULATOR_HOST="localhost:8080"
@@ -27,7 +32,7 @@ func main() {
 		"localhost:8080", // auth host users 8080
 		// For gRPC clients, it's convenient to support HTTP/2 without TLS. You can
 		// avoid x/net/http2 by using http.ListenAndServeTLS.
-		h2c.NewHandler(mux, &http2.Server{}),
+		h2c.NewHandler(newCORS().Handler(mux), &http2.Server{}),
 	)
 	log.Fatalf("listen failed: " + err.Error())
 }
@@ -48,4 +53,44 @@ func createNewService() *exampleservice.Service {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 	return exampleservice.NewService(auth, firestore)
+}
+
+func newCORS() *cors.Cors {
+	// To let web developers play with the demo service from browsers, we need a
+	// very permissive CORS setup.
+	return cors.New(cors.Options{
+		AllowedMethods: []string{
+			http.MethodHead,
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+		},
+		AllowOriginFunc: func(origin string) bool {
+			// Allow all origins, which effectively disables CORS.
+			return true
+		},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{
+			// Content-Type is in the default safelist.
+			"Accept",
+			"Accept-Encoding",
+			"Accept-Post",
+			"Connect-Accept-Encoding",
+			"Connect-Content-Encoding",
+			"Content-Encoding",
+			"Grpc-Accept-Encoding",
+			"Grpc-Encoding",
+			"Grpc-Message",
+			"Grpc-Status",
+			"Grpc-Status-Details-Bin",
+			"Access-Control-Allow-Origin",
+		},
+		// Let browsers cache CORS information for longer, which reduces the number
+		// of preflight requests. Any changes to ExposedHeaders won't take effect
+		// until the cached data expires. FF caps this value at 24h, and modern
+		// Chrome caps it at 2h.
+		MaxAge: int(2 * time.Hour / time.Second),
+	})
 }
