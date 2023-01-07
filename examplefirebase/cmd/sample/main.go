795499d03fafb893421efdba15c2088499ccf199
Liam Maguire <liammaguiresoftware@outlook.com>
2023-01-07 14:13:13 -0300

    Re in troduce cors.

--- examplefirebase/cmd/sample/main.go
+++ examplefirebase/cmd/sample/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
+	"time"
+
 	v4 "firebase.google.com/go/v4"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 	"google.golang.org/api/option"
-	"log"
-	"net/http"
 
 	// your protoPathHere
 	"github.com/lcmaguire/protoc-gen-go-goo/examplefirebase/sampleconnect"
@@ -27,7 +29,7 @@ func main() {
 		"localhost:8080", // auth host users 8080
 		// For gRPC clients, it's convenient to support HTTP/2 without TLS. You can
 		// avoid x/net/http2 by using http.ListenAndServeTLS.
-		h2c.NewHandler(mux, &http2.Server{}),
+		h2c.NewHandler(newCORS().Handler(mux), &http2.Server{}),
 	)
 	log.Fatalf("listen failed: " + err.Error())
 }
@@ -48,4 +50,44 @@ func createNewService() *exampleservice.Service {
 		log.Fatalf("error initializing app: %v\n", err)
 	}
 	return exampleservice.NewService(auth, firestore)
+}
+
+func newCORS() *cors.Cors {
+	// To let web developers play with the demo service from browsers, we need a
+	// very permissive CORS setup.
+	return cors.New(cors.Options{
+		AllowedMethods: []string{
+			http.MethodHead,
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+		},
+		AllowOriginFunc: func(origin string) bool {
+			// Allow all origins, which effectively disables CORS.
+			return true
+		},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{
+			// Content-Type is in the default safelist.
+			"Accept",
+			"Accept-Encoding",
+			"Accept-Post",
+			"Connect-Accept-Encoding",
+			"Connect-Content-Encoding",
+			"Content-Encoding",
+			"Grpc-Accept-Encoding",
+			"Grpc-Encoding",
+			"Grpc-Message",
+			"Grpc-Status",
+			"Grpc-Status-Details-Bin",
+			"Access-Control-Allow-Origin",
+		},
+		// Let browsers cache CORS information for longer, which reduces the number
+		// of preflight requests. Any changes to ExposedHeaders won't take effect
+		// until the cached data expires. FF caps this value at 24h, and modern
+		// Chrome caps it at 2h.
+		MaxAge: int(2 * time.Hour / time.Second),
+	})
 }
